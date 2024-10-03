0ca574e6a565da342637272ad0e86609d8fb3055
João Izídio <88620101+joaoizidioLoiola@users.noreply.github.com>
2024-10-02 22:22:03 -0300

    Update page component with new logo image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,24 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Image from "next/image";
 
 export default function page() {
   return (
     <>
       <Header />
       <main>
-        <h1 className="text-3xl">Lorem ipsum dolor</h1> sit amet consectetur
-        adipisicing elit. Suscipit, ipsum? Temporibus ullam ut recusandae
-        voluptatum hic atque quas pariatur omnis nesciunt! Esse eum possimus
-        maxime eius nihil at dolor alias.
+        <h1 className="text-3xl">
+          Lorem ipsum dolor
+          <Image
+            src={"/img/logoR.png"}
+            width={250}
+            height={250}
+            alt="Logo React"
+          />
+        </h1>{" "}
+        sit amet consectetur adipisicing elit. Suscipit, ipsum? Temporibus ullam
+        ut recusandae voluptatum hic atque quas pariatur omnis nesciunt! Esse
+        eum possimus maxime eius nihil at dolor alias.
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
           molestiae ipsa obcaecati sed, dicta consequatur nisi ut neque
